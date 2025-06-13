ca8de636a9961647f7c2a6a15dbc5a641fdabe5b
rodrigodiasb <62857786+rodrigodiasb@users.noreply.github.com>
2025-06-13 11:17:21 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,23 @@
-
-const CACHE_NAME = 'registro-vitima-cache-v1';
-const FILES_TO_CACHE = [
-  '.',
-  'index.html',
-  'manifest.json',
-];
-
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('[ServiceWorker] Precacheando arquivos');
-      return cache.addAll(FILES_TO_CACHE);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('avaliacao-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removendo cache antigo', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', evt => {
-  if (evt.request.mode !== 'navigate') {
-    return;
-  }
-  evt.respondWith(
-    caches.match(evt.request).then(response => {
-      return response || fetch(evt.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
